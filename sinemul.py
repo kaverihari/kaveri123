d9b2537487bf1c81ef2a5b6341337c482b59d646
kaverihari <47147310+kaverihari@users.noreply.github.com>
2019-01-30 19:10:34 +0530

    Add files via upload

---
+++ sinemul.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as p
+fs=int(input("enter sample frequency"))
+f1=int(input("enter first sine wave frequency"))
+f2=int(input("enter second sine wave frequency"))
+n=int(input("enter number of samples"))
+g=np.arange(n)
+x=np.sin(2*np.pi*f1/fs*g)
+p.subplot(311)
+p.plot(g,x)
+p.xlabel("samples")
+p.ylabel("amplitude")
+y=np.sin(2*np.pi*f2/fs*g)
+p.subplot(312)
+p.plot(g,y)
+p.xlabel("samples")
+p.ylabel("amplitude")
+c=x*y
+p.subplot(313)
+p.plot(g,c)
+p.xlabel("samples")
+p.ylabel("amplitude")
+p.show()
